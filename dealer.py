14580a0fd00eefccaa1ee2e8cb3ec13f1d1d28d2
jacksongayda <jacksongayda@gmail.com>
2023-01-10 20:52:39 -0800

    Add HouseRules class

--- dealer.py
+++ dealer.py
@@ -2,31 +2,39 @@ from shoe import Shoe
 from card import Card
 from hand import Hand
 from typing import List
-class Dealer:
-    def __init__(self, standValue, DASoffered, RSAoffered, LSoffered, penetration, shoeSize):
+
+class HouseRules:
+    def __init__(self, standValue, DASoffered, RSAoffered, LSoffered, doubleOnSoft7OrHigher):
         self.standValue = standValue
         self.DASoffered = DASoffered
         self.RSAoffered = RSAoffered
         self.LSoffered = LSoffered
+        self.doubleOnSoft7OrHigher = doubleOnSoft7OrHigher
+        print("HOUSE RULES:")
+        print("Dealer stands on ", standValue, " | Double after split offered? ", DASoffered, " | Players can re-split aces? ", RSAoffered, " Surrender offered? ", LSoffered)
+class Dealer:
+    def __init__(self, penetration, shoeSize, houseRules):
         self.penetration = penetration
         self.shoe = Shoe(shoeSize)
+        self.houseRules: HouseRules = houseRules
         self.hand: Hand = None
-    
-    def updateHand(self, hand: Hand):
-        self.hand = hand
-    
-    # def clearHand(self):
-    #     self.discardCards(self.hand)
-    #     self.hand = None
+        self.upcard: Card = None
+        self.losses = 0
+        self.gains = 0
     
     def dealCard(self):
         return self.shoe.drawCard()
+    
+    def discardDealersCards(self):
+        print("Discarding dealer hand: ", self.hand.cards)
+        for card in self.hand.cards:
+            self.shoe.discardCard(card)
+        self.hand = None
+        self.upcard = None
 
-    def discardCards(self, hand: Hand):
-        print(hand.cards)
+    def discardPlayersCards(self, hand: Hand, playerName):
+        print("Discarding ", playerName, "'s hand...")
         for card in hand.cards:
-            print("discarding card...")
-            card.printCard()
             self.shoe.discardCard(card)
     
     def deckPenetrationTooHigh(self):
@@ -38,10 +46,24 @@ class Dealer:
             print("Should be equal to: ", self.shoe.numDecks * 52)
         return (len(self.shoe.discard) + len(self.shoe.drawPile) == self.shoe.numDecks * 52)
     
+    def handlePayout(self, betSize: int, isBlackjack):
+        if isBlackjack:
+            self.losses += betSize * 1.5
+            return betSize * 1.5
+        self.losses += betSize
+        return betSize
+    
+    def insuranceIsOffered(self):
+        return self.upcard.getValue() == 1
+
+    def setUpCard(self, upcard: Card):
+        self.upcard = upcard
+    
     def shuffle(self):
         self.shoe.resetShoe()
-
-    # def discardCards(self, cards: List[Card]):
-    #     print("Discarding: ", type(cards))
-    #     for card in cards:
-    #         self.shoe.discardCard(card)
+    
+    def updateHand(self, hand: Hand):
+        self.hand = hand
+    
+    def updateGains(self, amount):
+        self.gains += amount
