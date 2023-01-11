ebd1386429664fad5a3992175c97b1cb956d1657
jacksongayda <jacksongayda@gmail.com>
2023-01-10 20:50:33 -0800

    Add class for 1-6 and 1-50 bet spreads, will maybe change later

--- bet.py
+++ bet.py
@@ -1,6 +1,36 @@
-class BetSpread:
+class BetSpreadInterface:
+    def getBetSpreads(trueCount: int, tableMin: int) -> int:
+        pass
+class spread1_6(BetSpreadInterface):
     def __init__(self):
-        print("Initializing bet spreads...")
+        print("Initializing 1-6 Bet Spread")
     
+    # 1-6 Bet Spread
     def getBetSpreads(trueCount, tableMin):
-        return 1
+        if trueCount < 0: return 100
+        if trueCount > 5: return 600
+        betSpread = {
+            0: 100,
+            1: 200,
+            2: 300,
+            3: 400,
+            4: 500,
+            5: 600
+        }
+
+        return betSpread.get(trueCount)
+
+class spread1_50(BetSpreadInterface):
+    def __init__(self):
+        print("Initializing 1-50 Bet Spread")
+    
+    def getBetSpreads(trueCount: int, tableMin: int):
+        if trueCount <= 0: return tableMin
+        if trueCount >= 5: return 500
+        betSpread = {
+            1: 100,
+            2: 200,
+            3: 300,
+            4: 400,
+        }
+        return betSpread.get(trueCount)
