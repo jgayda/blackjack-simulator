d7334efb5c675a17d16f2b7fd8b94edf7dc61e3a
jacksongayda <jacksongayda@gmail.com>
2023-01-10 20:53:00 -0800

    Lay foundation for new strategy classes

--- strategies.py
+++ strategies.py
@@ -1,23 +1,63 @@
 from hand import Hand
 from enum import Enum
+import random
 class GameActions(Enum):
     HIT = "H"
     STAND = "S"
     DOUBLE = "D"
-class Strategy:
-    def __init__(self):
-        self.name = "strat"
+class StrategyInterface:
+    def __init__(self, doubleAfterSplitOffered, isCounting):
+        pass
+    
+    def shouldSplitPair(self, pairValue: int, dealerUpcard: int) -> bool:
+        pass
 
-class RandomStrategy(Strategy):
-    def __init__(self):
+    def softTotalOptimalDecision(self, hand: Hand, dealerUpcard: int) -> GameActions:
+        pass
+    
+    def willTakeInsurance(self) -> None:
+        pass
+
+# RandomStrategy represents the strategy of a player who will leave every single game decision up to chance.
+# This goes without saying, do not attempt this strategy in a real life casino.
+class RandomStrategy(StrategyInterface):
+    def __init__(self, doubleAfterSplitOffered, isCounting):
+        self.doubleAfterSplitOffered = doubleAfterSplitOffered
+        self.isCounting = isCounting
         self.name = "random"
+    
+    def shouldSplitPair(self, pairValue: int, dealerUpcard: int) -> bool:
+        return bool(random.getrandbits(1))
+    
+    def softTotalOptimalDecision(self, hand: Hand, dealerUpcard: int) -> GameActions:
+        return random.choice([GameActions.HIT, GameActions.STAND, GameActions.DOUBLE])
+    
+    def willTakeInsurance(self):
+        return bool(random.getrandbits(1))
 
-class CasinoStrategy(Strategy):
-    def __init__(self):
+# CasinoStrategy represents the strategy of a player who will play exactly how the casino plays. In other words,
+# if the dealer was an actual player at the table this is who they'd be. 
+class CasinoStrategy(StrategyInterface):
+    def __init__(self, doubleAfterSplitOffered, isCounting):
         self.name = "casino"
+        self.doubleAfterSplitOffered = doubleAfterSplitOffered
+        self.isCounting = isCounting
+    
+    def shouldSplitPair(self, pairValue: int, dealerUpcard: int):
+        # Casinos never split pairs! 
+        return False
+    
+    def softTotalOptimalDecision(self, hand: Hand, dealerUpcard: int) -> GameActions:
+        return super().softTotalOptimalDecision(hand, dealerUpcard)
 
 # See https://www.blackjackapprenticeship.com/wp-content/uploads/2018/08/BJA_Basic_Strategy.jpg for charts
-class BasicStrategy(Strategy):
+class BasicStrategy(StrategyInterface):
+    def __init__(self, doubleAfterSplitOffered, isCounting):
+        self.doubleAfterSplitOffered = doubleAfterSplitOffered
+        self.isCounting = isCounting
+        if doubleAfterSplitOffered:
+            self.DASdeviations()
+    
     # Contains the optimal pair splitting strategy given the rank of the player's pair and the
     # dealer's upcard according to Basic Strategy principles. 
         #     2      3      4      5      6      7      8      9      10      A
@@ -63,24 +103,20 @@ class BasicStrategy(Strategy):
         9:  ["H", "D", "D", "D", "D", "H", "H", "H", "H", "H"],
         8:  ["H", "H", "H", "H", "H", "H", "H", "H", "H", "H"],
     }
-
     # Handles pair splitting deviations in the event that double-after-split (DAS) is offered.
     def DASdeviations(self):
         self.pairSplitting.update({6: [True, True,  True,  True,  True,  False, False, False, False, False]})
         self.pairSplitting.update({4: [False, False, False, True, True, False, False, False, False, False]})
         self.pairSplitting.update({3: [True, True, True,  True,  True,  True,  False, False, False, False]})
         self.pairSplitting.update({2: [True, True, True,  True,  True,  True,  False, False, False, False]})
-    
+
     def shouldSplitPair(self, pairValue: int, dealerUpcard: int):
         return self.pairSplitting.get(pairValue)[dealerUpcard - 2]
     
     def softTotalOptimalDecision(self, hand: Hand, dealerUpcard: int):
         chartVal: GameActions = self.softTotals.get(hand.getSoftTotalOtherCard())[dealerUpcard - 2]
         return chartVal
-
     
-    def __init__(self, doubleAfterSplitOffered, isCounting):
-        self.doubleAfterSplitOffered = doubleAfterSplitOffered
-        self.isCounting = isCounting
-        if doubleAfterSplitOffered:
-            self.DASdeviations()
+    def willTakeInsurance(self, runningCount):
+        # Perfect basic strategy never takes insurance unless running count is exceptionally high
+        return runningCount >= 8
